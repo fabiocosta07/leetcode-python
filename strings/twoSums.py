23a3626f73f17a854b5e205222d3076a91c95972
fabio <fabiocosta07@gmail.com>
2022-11-29 09:34:42 -0300

    improved

--- strings/twoSums.py
+++ strings/twoSums.py
@@ -10,32 +10,35 @@ class Solution(object):
         """
         dictNums = {}
         for n in range(0, len(nums)):
-            dictNums[nums[n]] = n
-        for n in range(0, len(nums)):
             diff = target - nums[n]
-            if diff in dictNums and n != dictNums[diff]:
+            if diff in dictNums:
                 return [n, dictNums[diff]]
+            else:
+                dictNums[nums[n]] = n
+
         return []
+
 
 class TestStringMethods(unittest.TestCase):
 
     def test_case1(self):
         sol = Solution()
-        res = sol.twoSum([2,7,11,15], 9)
-        self.assertEqual([0,1], res)
+        res = sol.twoSum([2, 7, 11, 15], 9)
+        self.assertIn(res, [[0, 1], [1, 0]])
 
     def test_case2(self):
         sol = Solution()
-        res = sol.twoSum([3,2,4], 6)
-        self.assertEqual([1,2], res)
+        res = sol.twoSum([3, 2, 4], 6)
+        self.assertIn(res, [[1, 2], [2, 1]])
 
     def test_case3(self):
         sol = Solution()
-        res = sol.twoSum([3,3], 6)
-        self.assertEqual([0,1], res)
+        res = sol.twoSum([3, 3], 6)
+        self.assertIn(res, [[0, 1], [1, 0]])
+
 
 if __name__ == '__main__':
     unittest.main()
 
-#time complexity 0(2n) => O(n)
-#space complexity ?
+# time complexity 0(2n) => O(n)
+# space complexity ?
