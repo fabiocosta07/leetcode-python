2861884b0137cd21ccc09398b6ab06fd8dc8de76
fabio <fabiocosta07@gmail.com>
2022-12-02 10:55:37 -0300

    add top k frequent elements

--- heap_priority_queue/topk_frequent_elements.py
+++ heap_priority_queue/topk_frequent_elements.py
@@ -15,10 +15,15 @@ class Solution:
         for n in nums:
             pq.put((-counter[n], n))
 
-        res = set()
-        for i in range(0, len(nums)):
-            res.add(pq.get()[1])
-        return list(res)[0: k]
+        res = []
+        most_freq = None
+        while not pq.empty():
+            freq_tuple = pq.get()
+            if most_freq != freq_tuple[1]:
+                most_freq = freq_tuple[1]
+                res.append(most_freq)
+
+        return res[0: k]
 
 
 class TestMethods(unittest.TestCase):
@@ -32,6 +37,11 @@ class TestMethods(unittest.TestCase):
         sol = Solution()
         res = sol.topKFrequent([1], 1)
         self.assertIn(res, [[1]])
+
+    def test_case3(self):
+        sol = Solution()
+        res = sol.topKFrequent([4, 1, -1, 2, -1, 2, 3], 2)
+        self.assertIn(res, [[-1, 2]])
 
 
 if __name__ == '__main__':
