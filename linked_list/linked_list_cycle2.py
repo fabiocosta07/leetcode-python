adcaf237807671d4a132ce7ac50dd6e319e23ea6
fabio <fabiocosta07@gmail.com>
2022-12-09 08:32:26 -0300

    linked list cycle 2

---
+++ linked_list/linked_list_cycle2.py
@@ -0,0 +1,51 @@
+import unittest
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return None
+        pathMap = {}
+        nextNode = head
+        while nextNode.next != None:
+            if nextNode in pathMap:
+                return pathMap[nextNode]
+            pathMap[nextNode] = nextNode
+            nextNode = nextNode.next
+        return None
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_case1(self):
+        sol = Solution()
+        res = sol.isValid("()")
+        self.assertTrue(res)
+
+    def test_case2(self):
+        sol = Solution()
+        res = sol.isValid("()[]{}")
+        self.assertTrue(res)
+
+    def test_case3(self):
+        sol = Solution()
+        res = sol.isValid("(]")
+        self.assertFalse(res)
+
+    def test_case4(self):
+        sol = Solution()
+        res = sol.isValid("]")
+        self.assertFalse(res)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# time complexity 0(2n) => O(n)
+# space complexity ?
