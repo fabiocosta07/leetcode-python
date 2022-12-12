443c807ac709d8cf5b8d1fad7f4a1ac00ee509ef
fabio <fabiocosta07@gmail.com>
2022-12-12 11:07:57 -0300

    add linked list cycle 2 sol 2

--- linked_list/linked_list_cycle2.py
+++ linked_list/linked_list_cycle2.py
@@ -17,6 +17,18 @@ class Solution:
             nextNode = nextNode.next
         return None
 
+    def detectCycle_sol2(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        next_node_slow = head
+        next_node_fast = head
+        while next_node_slow \
+                and next_node_fast \
+                and next_node_fast.next:
+            next_node_slow = next_node_slow.next
+            next_node_fast = next_node_fast.next.next
+            if next_node_fast is next_node_slow:
+                return next_node_fast
 class TestStringMethods(unittest.TestCase):
 
     def test_case1(self):
@@ -32,6 +44,21 @@ class TestStringMethods(unittest.TestCase):
     def test_case3(self):
         sol = Solution()
         res = sol.detectCycle(build_input_list([1], -1))
+        self.assertFalse(res)
+
+    def test_case1_sol2(self):
+        sol = Solution()
+        res = sol.detectCycle_sol2(build_input_list([3, 2, 0, -4], 1))
+        self.assertTrue(res)
+
+    def test_case2_sol2(self):
+        sol = Solution()
+        res = sol.detectCycle_sol2(build_input_list([1, 2], 0))
+        self.assertTrue(res)
+
+    def test_case3_sol2(self):
+        sol = Solution()
+        res = sol.detectCycle_sol2(build_input_list([1], -1))
         self.assertFalse(res)
 
 
