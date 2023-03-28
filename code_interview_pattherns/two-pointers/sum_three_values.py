2ba9a206c6aedaf6b68a353b613fb9d8f25a77f6
guest <fabiocosta07@gmail.com>
2023-03-27 22:54:12 -0300

    update

--- code_interview_pattherns/two-pointers/sum_three_values.py
+++ code_interview_pattherns/two-pointers/sum_three_values.py
@@ -3,13 +3,21 @@ import unittest
 
 class Solution:
     def find_sum_of_three(self, nums, target):
-        s_nums = nums.sort()
+        nums.sort()
 
-        p1 = 0
-        p2 = len(s_nums) - 1
+        for i in range(len(nums)):
+            p1 = i + 1
+            p2 = len(nums) - 1
 
-        for i in range(len(s_nums)):
-            return False
+            while p1 < p2:
+                three_sum = nums[i] + nums[p1] + nums[p2]
+                if three_sum == target:
+                    return True
+                if three_sum > target:
+                    p2 -= 1
+                else:
+                    p1 += 1
+        return False
 
 
 class TestStringMethods(unittest.TestCase):
@@ -18,3 +26,9 @@ class TestStringMethods(unittest.TestCase):
         sol = Solution()
         res = sol.find_sum_of_three([3, 7, 1, 2, 8, 4, 5], 10)
         self.assertTrue(res)
+
+
+    def test_case1(self):
+        sol = Solution()
+        res = sol.find_sum_of_three([1, -1, 0] , -1)
+        self.assertFalse(res)
