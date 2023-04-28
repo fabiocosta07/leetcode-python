05fc751fb200530869f03b5850618fe149b1a824
guest <fabiocosta07@gmail.com>
2023-04-28 10:06:55 -0300

    update

---
+++ trees/main.py
@@ -0,0 +1,17 @@
+import unittest
+from typing import Optional
+from trees.tree import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left_count = self.maxDepth(root.left)
+        right_count = self.maxDepth(root.right)
+        return max(left_count, right_count) + 1
+
+class TestMethods(unittest.TestCase):
+
+    def test_case1(self):
+        self.assertTrue()
